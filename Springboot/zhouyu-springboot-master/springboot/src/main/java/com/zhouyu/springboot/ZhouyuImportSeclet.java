bb967893e7daca358db7fd8b86c8266153f3bb54
hanhan167 <375441493@qq.com>
2022-07-17 21:41:49 +0800

    123

---
+++ Springboot/zhouyu-springboot-master/springboot/src/main/java/com/zhouyu/springboot/ZhouyuImportSeclet.java
@@ -0,0 +1,26 @@
+package com.zhouyu.springboot;
+
+import org.springframework.context.annotation.DeferredImportSelector;
+import org.springframework.context.annotation.ImportSelector;
+import org.springframework.core.type.AnnotationMetadata;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.ServiceLoader;
+
+public class ZhouyuImportSeclet implements ImportSelector {
+
+    @Override
+    public String[] selectImports(AnnotationMetadata importingClassMetadata) {
+        // 自动配置
+        ServiceLoader<AutoConfiguration> loader = ServiceLoader.load(AutoConfiguration.class);
+
+        List<String> list = new ArrayList<>();
+
+        for (AutoConfiguration configuration : loader) {
+            list.add(configuration.getClass().getName());
+        }
+
+        return list.toArray(new String[0]);
+    }
+}
